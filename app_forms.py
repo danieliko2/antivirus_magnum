ae8e7454860367d7014aeb1b17cb81bbdcfd3db3
jenkins <jenkins@daniel>
2022-08-17 17:41:39 +0300

    signup, login, logout implemented

---
+++ app_forms.py
@@ -0,0 +1,14 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Length
+
+
+
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = StringField('password', validators=[InputRequired(), Length(min=8, max=60)])
+    remember = BooleanField('remember me')
+
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
+    password = StringField('password', validators=[InputRequired(), Length(min=8, max=60)])
